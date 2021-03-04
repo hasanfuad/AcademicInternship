3b9c63956dfd4724b702f1e0083c6f96f0238c43
hasanfuad <hasanmdfuad@hotmail.com>
2021-03-05 00:36:02 +0600

    commit collectionTypeTesting

---
+++ Class 2/Task/src/com/company/CollectionTypeTesting.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class CollectionTypeTesting {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("hahahah");
+        list.add("23");
+        System.out.println(list);
+
+
+    }
+}
