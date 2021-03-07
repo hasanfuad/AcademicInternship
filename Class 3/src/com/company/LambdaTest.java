858d7c80f24e05ae78053ce29e11759298393c34
hasanfuad <hasanmdfuad@hotmail.com>
2021-03-08 00:58:46 +0600

    commit more lambda features methods

--- Class 3/src/com/company/LambdaTest.java
+++ Class 3/src/com/company/LambdaTest.java
@@ -1,5 +1,10 @@
 package com.company;
 
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LambdaTest {
     public static void main(String[] args) {
         new Thread(new Runnable() {
@@ -14,5 +19,45 @@ public class LambdaTest {
         new Thread(() -> {
             System.out.println("After Java8 example-2");
         }).start();
+
+        //Prior Java 8
+
+        List<String> list = Arrays.asList("lambda","Practice","2021","3","7");
+
+        for (String elem:list) {
+            System.out.println(elem);
+        }
+        System.out.println("=============== After Java-8 =======");
+        list.forEach((res) -> System.out.println(res));
+        // More shorter
+        System.out.println("============ More shorter ==============");
+        list.forEach(System.out::println);
+
+        //Before Java 8
+
+//        List <String> language = Arrays.asList("Java","Scala","C++","Haskell", "Lisp");
+//        for (String elem: language){
+//            System.out.println("Lan: "+elem);
+//        }
+
+        List<String> list1 = Arrays.asList("xyz","abc","nothing","lovely") ;
+//        for (String elem: list1){
+//            System.out.println("Lan: "+elem);
+//        }
+        //Using lambda
+        System.out.println("=========== Using Lambda =============");
+        List<String> lanList = list1.stream()
+                .map(lan->"Lan: "+lan)
+                .collect(Collectors.toList());
+        System.out.println(lanList);
+
+        Set<Integer> prices = new HashSet<>(Arrays.asList(50,23,20,30,50,70));
+        Optional<Integer> processedPrice = prices.stream()
+                .filter(price -> price > 20)
+                .map(price -> price + 100)
+                .reduce((priceA,priceB) -> priceA+priceB);
+//                .max((priceA,priceB) -> priceA - priceB);
+                //.collect(Collectors.toSet());
+        System.out.println(processedPrice.get());
     }
 }
